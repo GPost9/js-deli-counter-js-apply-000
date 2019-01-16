a90cf1f2365f5dee4236c41652da8bb9f6082971
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-16 20:53:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber (line, person){
+  var katzDeli= ["Ada", "Grace", "Kent"];
+  var
+  let i = 0;
+  for(i = 1; i< katzDeli.length; i++) {
+    array.push(`Welcome, ${katzDeli}. You are number ${[i]} in line.`);
+    
+  }
+  
+  return katzDeli;
+}
+
